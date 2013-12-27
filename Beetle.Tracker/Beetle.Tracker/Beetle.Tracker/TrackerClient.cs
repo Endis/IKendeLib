f9d412d6d4bb566a61717a85082ce20dc82c9fb1
Henryfan <henryfan@msn.com>
2013-12-27 14:18:21 +0800

    Tracker Client

--- Beetle.Tracker/Beetle.Tracker/Beetle.Tracker/TrackerClient.cs
+++ Beetle.Tracker/Beetle.Tracker/Beetle.Tracker/TrackerClient.cs
@@ -15,6 +15,17 @@ namespace Beetle.Tracker
 
         private Beetle.Clients.TcpSyncNode mNode;
 
-        
+        public AppHost GetHost(string app,IProperties properties =null)
+        {
+            if(properties ==null)
+            {
+                properties = new Properties();
+            }
+            HttpExtend.HttpHeader request = Protocol.Get(app, properties);
+            HttpExtend.HttpHeader result = mNode.Send<HttpExtend.HttpHeader>(request);
+            if (result.RequestType == "500")
+                throw new Exception(result.Url);
+            return new AppHost { Host=result["Host"],Port=int.Parse(result["Port"]) };
+        }
     }
 }
