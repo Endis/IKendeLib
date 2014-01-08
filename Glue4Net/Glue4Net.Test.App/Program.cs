1f0bda4a1f7db8257bf7fab0774471606e96059d
Henryfan <henryfan@msn.com>
2014-01-08 16:00:44 +0800

    Glue4Net
    Glue4Net

---
+++ Glue4Net/Glue4Net.Test.App/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glue4Net.Test.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DomainAdapter da = new DomainAdapter(@"C:\Users\Administrator\Documents\GitHub\IKendeLib\Glue4Net\Glue4Net.Test.Demo\bin\Debug",
+                "TEST");
+            da.Log = new ConsoleLoger();
+            da.Load();
+            Demo.ITest test = (Demo.ITest)da.CreateProxyObject("test");
+            int i = test.Add(2);
+            Console.WriteLine(i);
+            Console.Read();
+        }
+    }
+    public class ConsoleLoger : MarshalByRefObject, IEventLog
+    {
+
+        public void Track(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public void Track(string formater, params object[] data)
+        {
+            Console.WriteLine(formater,data);
+        }
+
+        public void Debug(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public void Debug(string formater, params object[] data)
+        {
+            Console.WriteLine(formater,data);
+        }
+
+        public void Info(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public void Info(string formater, params object[] data)
+        {
+            Console.WriteLine(formater,data);
+        }
+
+        public void Error(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public void Error(string formater, params object[] data)
+        {
+            Console.WriteLine(formater,data);
+        }
+        public override object InitializeLifetimeService()
+        {
+            return null;
+        }
+    }
+}
---
+++ Glue4Net/Glue4Net/DomainAdapter.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Glue4Net
+{
+    public class DomainAdapter
+    {
+
+        public DomainAdapter(string appPath, string appName)
+        {
+            if (appPath.LastIndexOf(System.IO.Path.DirectorySeparatorChar) != appPath.Length - 1)
+            {
+                appPath += System.IO.Path.DirectorySeparatorChar;
+            }
+            AppPath = appPath;
+            CachePath = Path.Combine(AppPath, "_tempdll" + Path.DirectorySeparatorChar);
+            AppName = appName;
+        }
+
+        private AssemblyLoader mLoader;
+
+        private AppDomain mAppDomain;
+
+        public string AppName
+        {
+            get;
+            set;
+        }
+
+        public string CachePath
+        {
+            get;
+            set;
+        }
+
+        public string AppPath
+        { get; set; }
+
+        public IEventLog Log
+        {
+            get;
+            set;
+        }
+
+        public void Load()
+        {
+            try
+            {
+                Type loadertype = typeof(AssemblyLoader);
+                AppDomainSetup setup = new AppDomainSetup();
+                setup.ApplicationName = AppName;
+                setup.ApplicationBase = AppPath;
+                setup.CachePath = CachePath;
+                setup.ShadowCopyFiles = "true";
+                setup.ShadowCopyDirectories = AppPath;
+                setup.ConfigurationFile = AppPath + "app.config";
+                mAppDomain = AppDomain.CreateDomain(
+                   AppPath, null, setup);
+                mLoader = (AssemblyLoader)mAppDomain.CreateInstanceAndUnwrap(
+                    loadertype.Assembly.GetName().Name,
+                    loadertype.FullName);
+                mLoader.Log = Log;
+                mLoader.AppName = AppName;
+                mLoader.LoadAssembly(AppPath);
+                mLoader.Load();
+                Log.Error("load {0} appdomain success!", AppName);
+            }
+            catch (Exception e_)
+            {
+                if (Log != null)
+                {
+                    Log.Error("load {0} appdomain error {1}!", AppName,e_.Message);
+                }
+            }
+        }
+
+        public void UnLoad()
+        {
+        
+            if (mLoader != null)
+            {
+                try
+                {
+                    mLoader.UnLoad();
+                    System.Threading.Thread.Sleep(1000);
+                    AppDomain.Unload(mAppDomain);
+                }
+                catch (Exception e_)
+                {
+                    if (Log != null)
+                    {
+                        Log.Error("unload {0} appdomain error {1}!", AppName,e_.Message);
+                    }
+                }
+                mLoader = null;
+            }
+        }
+
+        public object CreateProxyObject(string name)
+        {
+            if (mLoader != null)
+                return mLoader.CreateProxyObject(name);
+            return null;
+        }
+
+        public object GetValue(string key)
+        {
+            if (mLoader != null)
+                return mLoader.GetValue(key);
+            return null;
+        }
+
+        public void SetValue(string key, object value)
+        {
+            if (mLoader != null)
+                mLoader.SetValue(key, value);
+        }
+    }
+}
