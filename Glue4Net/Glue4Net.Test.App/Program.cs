2f63e03e35a12f45a178fa03fbbf8cb75252ff1a
Henryfan <henryfan@msn.com>
2014-01-13 16:05:34 +0800

    Glue4Net

--- Glue4Net/Glue4Net.Test.App/Program.cs
+++ Glue4Net/Glue4Net.Test.App/Program.cs
@@ -10,62 +10,14 @@ namespace Glue4Net.Test.App
         static void Main(string[] args)
         {
             
-           
-            DomainAdapter da = new DomainAdapter(@"C:\Users\Administrator\Documents\GitHub\IKendeLib\Glue4Net\Glue4Net.Test.Demo\bin\Debug",
-                "TEST",null);
-            da.Log = new ConsoleLoger();
+
+            DomainAdapter da = new DomainAdapter(@"C:\Test",
+                "TEST", new DomainArgs { Compiler=true, UpdateWatch= true, WatchFilter=new string[]{"*.cs"} });
+            da.Log = new ConsoleEventLogImpl();
             da.Load();
-            Demo.ITest test = (Demo.ITest)da.CreateProxyObject("test");
-            int i = test.Add(2);
-            Console.WriteLine(i);
+          
             Console.Read();
         }
     }
-    public class ConsoleLoger : MarshalByRefObject, IEventLog
-    {
-
-        public void Track(string value)
-        {
-            Console.WriteLine(value);
-        }
-
-        public void Track(string formater, params object[] data)
-        {
-            Console.WriteLine(formater,data);
-        }
-
-        public void Debug(string value)
-        {
-            Console.WriteLine(value);
-        }
-
-        public void Debug(string formater, params object[] data)
-        {
-            Console.WriteLine(formater,data);
-        }
-
-        public void Info(string value)
-        {
-            Console.WriteLine(value);
-        }
-
-        public void Info(string formater, params object[] data)
-        {
-            Console.WriteLine(formater,data);
-        }
-
-        public void Error(string value)
-        {
-            Console.WriteLine(value);
-        }
-
-        public void Error(string formater, params object[] data)
-        {
-            Console.WriteLine(formater,data);
-        }
-        public override object InitializeLifetimeService()
-        {
-            return null;
-        }
-    }
+   
 }
